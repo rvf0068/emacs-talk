439372e6f150f607bad7c83e7c8bd59681fa4c1b
Rafael Villarroel <rvf0068@gmail.com>
2018-01-20 15:50:54 -0600

    Add sage and GAP mode

--- talk.el
+++ talk.el
@@ -1,22 +1,26 @@
 (require 'demo-it)
 
 (setq org-tree-slide-slide-in-effect nil)
-(setq demo-it--presentation-hide-org-markers t)
 
 (demo-it-create :single-window
 		:fullscreen
 		:text-xx-large
 		:show-block-headers
 		:fixed-width
-		:show-org-markers
 		:advance-mode
                 (demo-it-presentation "talk.org") ;; C-> advances C-< backs down
 		demo-it-presentation-advance
 		magit-status
 		demo-it-presentation-return
 		demo-it-presentation-advance
-
-		;; delete-window
+		demo-it-presentation-advance
+		(demo-it-load-file "sage-mode-example.sage")
+		(demo-it-insert "### A graph\nG=graphs.PetersenGraph()\nG.show()\n" :instant)
+		delete-window
+		demo-it-presentation-advance
+		"M-x gap"
+		delete-window
+		demo-it-presentation-advance
 		;; (demo-it-load-file "elisp-example.el" :none)
 		;; ;; Start coding an Emacs Lisp function:
 		;; (demo-it-insert "addtopath")       ; Begin yasnippet template
