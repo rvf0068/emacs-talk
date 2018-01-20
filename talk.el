2a1013795440835739d4024723ae430103987ccf
Rafael Villarroel <rvf0068@gmail.com>
2018-01-20 16:28:08 -0600

    Reorganize
    First magit, sage, GAP, then org.

--- talk.el
+++ talk.el
@@ -12,14 +12,14 @@
 		demo-it-presentation-advance
 		magit-status
 		demo-it-presentation-return
-		demo-it-presentation-advance
-		demo-it-presentation-advance
 		(demo-it-load-file "sage-mode-example.sage")
 		(demo-it-insert "### A graph\nG=graphs.PetersenGraph()\nG.show()\n" :instant)
 		delete-window
 		demo-it-presentation-advance
 		"M-x gap"
 		delete-window
+		demo-it-presentation-advance
+		demo-it-presentation-advance
 		demo-it-presentation-advance
 		;; (demo-it-load-file "elisp-example.el" :none)
 		;; ;; Start coding an Emacs Lisp function:
